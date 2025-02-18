29edf26a1c31d86b0d4ced045391639e110790a9
NickDDDDDD <101779199+NickDDDDDD@users.noreply.github.com>
2025-02-18 21:51:19 +1100

    continue debug for mobile dragover

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+
 import Kanban from "./components/Kanban";
 function App() {
+  useEffect(() => {
+    const handleTouchMove = (e) => {
+      if (e.cancelable) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("touchmove", handleTouchMove, { passive: false });
+
+    return () => {
+      document.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
+
   return (
     <>
       <Kanban />
